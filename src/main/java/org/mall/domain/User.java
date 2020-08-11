f2469cbfa3adafa0fc52889db4c31bbd0dc42ddb
sunxuehao <sunxuehao0910Q@163.com>
2020-08-11 15:18:13 +0800

    更新--123

---
+++ src/main/java/org/mall/domain/User.java
@@ -0,0 +1,53 @@
+package org.mall.domain;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String userSex;
+
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userSex='" + userSex + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ target/classes/web/user/index.ftl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>首页</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <#include "../common/common.ftl" />
+</head>
+<body>
+<div>successful
+</div>
+</body>
+</html>
